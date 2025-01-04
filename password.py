20300fbbaef195ab4a3a85c6f29fdfecac5e130a
joshuaharris07 <joshua.harris07@gmail.com>
2025-01-04 00:59:40 -0700

    Delete password.py

--- password.py
+++
@@ -1,8 +0,0 @@
-my_password = "M2PolarBear$!"
-
-# @app.route('/customers/<email>', methods = ['GET']) # Will search emails ilike is similar to the emails in the list.
-# def query_customer_by_email(email):                 # TODO verify this function works. Maybe get rid of it, it isn't asked for?
-#     customers = Customer.query.filter(Customer.email.ilike(f"%{email}%")).all()
-#     if not customers:
-#         return jsonify({"error": "No customers found matching that email"})
-#     return customers_schema.jsonify(customers)
